267041bb07e1b27be0b9832a2f5f8c4597130e2e
zianglei <zianglei@126.com>
2019-08-11 11:49:14 +0800

    typing-test problem 02 finished

--- typing_test/typing_test.py
+++ typing_test/typing_test.py
@@ -16,12 +16,38 @@ def lines_from_file(path):
 def new_sample(path, i):
     if type(path) != str: return
     if i < 0: return
-    
+
     line_list = lines_from_file(path)
     if (i >= len(line_list)): return
     return line_list[i]
 
-            
+def calc_words_per_minute(typed_string, start_time, end_time):
+    words_length = len(typed_string) / 5
+    time = (end_time - start_time) / 60
+    return words_length / time
+
+def calc_accuracy_percentage(sample_paragraph, typed_string):
+    sample_words = split(strip(sample_paragraph))
+    typed_words = split(strip(typed_string))
+    length = min(len(sample_words), len(typed_words))
+    if length == 0: return 0.0
+
+    accuracy = 0
+    for i in range(length):
+        if sample_words[i] == typed_words[i]:
+            accuracy += 1
+    
+    return accuracy * 100/ length
+
+def analyze(sample_paragraph, typed_string, start_time, end_time):
+    """This function outputs a list containing two values: words per minute and accuracy percentage
+    start_time and end_time are measured in seconds
+    """
+    
+    result = []
+    result.append(calc_words_per_minute(typed_string, start_time, end_time))
+    result.append(calc_accuracy_percentage(sample_paragraph, typed_string))
+    return result
 # END Q1-5
 
 # Question 6
