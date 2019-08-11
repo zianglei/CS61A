112c9076af47d56282526f5cb219f3a8a3a97883
zianglei <zianglei@126.com>
2019-08-12 00:49:13 +0800

    typing-test all problems finished

--- typing_test/typing_test.py
+++ typing_test/typing_test.py
@@ -5,6 +5,8 @@ from ucb import main
 
 # BEGIN Q1-5
 "*** YOUR CODE HERE ***"
+
+
 def lines_from_file(path):
     if type(path) != str: return
     lines = []
@@ -12,6 +14,7 @@ def lines_from_file(path):
         for line in f.readlines():
             lines.append(line.strip())
     return lines
+
 
 def new_sample(path, i):
     if type(path) != str: return
@@ -21,10 +24,12 @@ def new_sample(path, i):
     if (i >= len(line_list)): return
     return line_list[i]
 
+
 def calc_words_per_minute(typed_string, start_time, end_time):
     words_length = len(typed_string) / 5
     time = (end_time - start_time) / 60
     return words_length / time
+
 
 def calc_accuracy_percentage(sample_paragraph, typed_string):
     sample_words = split(strip(sample_paragraph))
@@ -36,18 +41,20 @@ def calc_accuracy_percentage(sample_paragraph, typed_string):
     for i in range(length):
         if sample_words[i] == typed_words[i]:
             accuracy += 1
-    
-    return accuracy * 100/ length
+
+    return accuracy * 100 / length
+
 
 def analyze(sample_paragraph, typed_string, start_time, end_time):
     """This function outputs a list containing two values: words per minute and accuracy percentage
     start_time and end_time are measured in seconds
     """
-    
+
     result = []
     result.append(calc_words_per_minute(typed_string, start_time, end_time))
     result.append(calc_accuracy_percentage(sample_paragraph, typed_string))
     return result
+
 
 def pig_latin(word):
     if type(word) != str: return
@@ -56,22 +63,33 @@ def pig_latin(word):
         if not word[i].isalpha(): continue
         if word[i] in ['a', 'e', 'i', 'o', 'u']: break
 
-    if i == 0: newword = word + "way"
-    elif i == len(word) - 1: newword = word + "ay"
+    if i == 0:
+        newword = word + "way"
+    elif i == len(word) - 1:
+        newword = word + "ay"
     else:
         newword = lower(word[i:]) + lower(word[0: i]) + 'ay'
-    if ord('A') <= ord(word[0]) <= ord('Z'): return newword.capitalize()
-    else: return newword
+    if ord('A') <= ord(word[0]) <= ord('Z'):
+        return newword.capitalize()
+    else:
+        return newword
+
 
 def autocorrect(user_input, words_list, score_function):
-    if user_input in words_list: return user_input
+    if user_input in words_list:
+        return user_input
     else:
         return min(words_list, key=lambda x: score_function(x, user_input))
 
+
 def swap_score(string_one, string_two):
     if len(string_one) == 0 or len(string_two) == 0: return 0
-    if string_one[0] != string_two[0]: return 1 + swap_score(string_one[1:], string_two[1:])
-    else: return swap_score(string_one[1:], string_two[1:])
+    if string_one[0] != string_two[0]:
+        return 1 + swap_score(string_one[1:], string_two[1:])
+    else:
+        return swap_score(string_one[1:], string_two[1:])
+
+
 # END Q1-5
 
 # Question 6
@@ -79,26 +97,62 @@ def swap_score(string_one, string_two):
 def score_function(word1, word2):
     """A score_function that computes the edit distance between word1 and word2."""
 
-    if ______________: # Fill in the condition
+    if word1 == "" or word2 == "":
+        return max(len(word1), len(word2))
+
+    elif word1[0] == word2[0]:  # Feel free to remove or add additional cases
         # BEGIN Q6
-        "*** YOUR CODE HERE ***"
+        return score_function(word1[1:], word2[1:])
         # END Q6
 
-    elif ___________: # Feel free to remove or add additional cases
-        # BEGIN Q6
-        "*** YOUR CODE HERE ***"
-        # END Q6
-    
     else:
-        add_char = ______________  # Fill in these lines
-        remove_char = ______________ 
-        substitute_char = ______________ 
+        add_char = score_function(word1, word2[1:])  # Fill in these lines
+        remove_char = score_function(word1[1:], word2)
+        substitute_char = score_function(word1[1:], word2[1:])
         # BEGIN Q6
-        "*** YOUR CODE HERE ***"
         # END Q6
+        return 1 + min(add_char, remove_char, substitute_char)
+
 
 KEY_DISTANCES = get_key_distances()
 
+
 # BEGIN Q7-8
-"*** YOUR CODE HERE ***"
+def score_function_accurate(word1, word2):
+    if word1 == "" or word2 == "":
+        return max(len(word1), len(word2))
+    elif word1[0] == word2[0]:  # Feel free to remove or add additional cases
+        return score_function_accurate(word1[1:], word2[1:])
+    else:
+        add_char = 1 + score_function_accurate(word1, word2[1:])  # Fill in these lines
+        remove_char = 1 + score_function_accurate(word1[1:], word2)
+        substitute_char = KEY_DISTANCES[word2[0], word1[0]] + score_function_accurate(word1[1:], word2[1:])
+
+        return min(add_char, remove_char, substitute_char)
+
+
+score_mem = {}
+
+def score_function_final(word1, word2):
+    if word1 == "" or word2 == "":
+        return max(len(word1), len(word2))
+
+    if (word1, word2) in score_mem.keys():
+        return score_mem[word1, word2]
+    elif (word2, word1) in score_mem.keys():
+        return score_mem[word2, word1]
+
+    elif word1[0] == word2[0]:
+        return score_function_final(word1[1:], word2[1:])
+    else:
+        add_char = 1 + score_function_final(word1, word2[1:])  # Fill in these lines
+        remove_char = 1 + score_function_final(word1[1:], word2)
+        substitute_char = KEY_DISTANCES[word2[0], word1[0]] + score_function_final(word1[1:], word2[1:])
+
+        accuracy = min(add_char, remove_char, substitute_char)
+        score_mem[word1, word2] = accuracy
+        score_mem[word2, word1] = accuracy
+
+        return accuracy
+
 # END Q7-8
